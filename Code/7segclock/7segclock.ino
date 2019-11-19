a5397243e2af2ed7ca97cf2eade35e242a142af9
Anirudh Swarankar <swarnakar.ani24@gmail.com>
2019-11-19 19:04:10 +0530

    updating timing and cleaning of code.
    Preparing to port the code for V2 that is using esp8266 capabilities to sync time from either a device or ntp server and also set alarms via app remotely.

--- Code/7segclock/7segclock.ino
+++ Code/7segclock/7segclock.ino
@@ -16,102 +16,103 @@
 #define c6 11
 
 RTClib RTC;
-int num_array[10][7] = {  { 1,1,1,1,1,1,0 },    // 0
-                          { 0,1,1,0,0,0,0 },    // 1
-                          { 1,1,0,1,1,0,1 },    // 2
-                          { 1,1,1,1,0,0,1 },    // 3
-                          { 0,1,1,0,0,1,1 },    // 4
-                          { 1,0,1,1,0,1,1 },    // 5
-                          { 1,0,1,1,1,1,1 },    // 6
-                          { 1,1,1,0,0,0,0 },    // 7
-                          { 1,1,1,1,1,1,1 },    // 8
-                          { 1,1,1,1,0,1,1 }};   // 9
+int num_array[10][7] = {{1, 1, 1, 1, 1, 1, 0},  // 0
+                        {0, 1, 1, 0, 0, 0, 0},  // 1
+                        {1, 1, 0, 1, 1, 0, 1},  // 2
+                        {1, 1, 1, 1, 0, 0, 1},  // 3
+                        {0, 1, 1, 0, 0, 1, 1},  // 4
+                        {1, 0, 1, 1, 0, 1, 1},  // 5
+                        {1, 0, 1, 1, 1, 1, 1},  // 6
+                        {1, 1, 1, 0, 0, 0, 0},  // 7
+                        {1, 1, 1, 1, 1, 1, 1},  // 8
+                        {1, 1, 1, 1, 0, 1, 1}}; // 9
 
+void setup()
+{
 
-void setup() {
-  // put your setup code here, to run once:
-pinMode(a,OUTPUT);
-pinMode(b,OUTPUT);
-pinMode(c,OUTPUT);
-pinMode(d,OUTPUT);
-pinMode(e,OUTPUT);
-pinMode(f,OUTPUT);
-pinMode(g,OUTPUT);
-pinMode(c1,OUTPUT);
-pinMode(c2,OUTPUT);
-pinMode(c3,OUTPUT);
-pinMode(c4,OUTPUT);
-pinMode(c5,OUTPUT);
-pinMode(c6,OUTPUT);
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(c1, OUTPUT);
+  pinMode(c2, OUTPUT);
+  pinMode(c3, OUTPUT);
+  pinMode(c4, OUTPUT);
+  pinMode(c5, OUTPUT);
+  pinMode(c6, OUTPUT);
 
-digitalWrite(c1,LOW);
-digitalWrite(c2,LOW);
-digitalWrite(c3,LOW);
-digitalWrite(c4,LOW);
-digitalWrite(c5,LOW);
-digitalWrite(c6,LOW);
+  digitalWrite(c1, LOW);
+  digitalWrite(c2, LOW);
+  digitalWrite(c3, LOW);
+  digitalWrite(c4, LOW);
+  digitalWrite(c5, LOW);
+  digitalWrite(c6, LOW);
 
-digitalWrite(a,HIGH);
-digitalWrite(b,HIGH);
-digitalWrite(c,HIGH);
-digitalWrite(d,HIGH);
-digitalWrite(e,HIGH);
-digitalWrite(f,HIGH);
-digitalWrite(g,HIGH);
-
-//Serial.begin(9600);
-
- Wire.begin();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  Wire.begin();
 }
-void printno(int pos , int no){
+void printno(int pos, int no)
+{
 
-  
   int p;
-  
-  switch(pos)
+
+  switch (pos)
   {
-    case 1:p=14;break;
-    case 2:p=15;break;
-    case 3:p=16;break;
-    case 4:p=17;break;
-    case 5:p=10;break;
-    case 6:p=11;break;
-    }
-    
-    digitalWrite(p,HIGH);
-
-    digitalWrite(a,!num_array[no][0]);
-    digitalWrite(b,!num_array[no][1]);
-    digitalWrite(c,!num_array[no][2]);
-    digitalWrite(d,!num_array[no][3]);
-    digitalWrite(e,!num_array[no][4]);
-    digitalWrite(f,!num_array[no][5]);
-    digitalWrite(g,!num_array[no][6]);
-    delay(3);  
-    digitalWrite(p,LOW); 
-     
-   
-  
-  
+  case 1:
+    p = 14;
+    break;
+  case 2:
+    p = 15;
+    break;
+  case 3:
+    p = 16;
+    break;
+  case 4:
+    p = 17;
+    break;
+  case 5:
+    p = 10;
+    break;
+  case 6:
+    p = 11;
+    break;
   }
+  digitalWrite(p, HIGH);
+  digitalWrite(a, !num_array[no][0]);
+  digitalWrite(b, !num_array[no][1]);
+  digitalWrite(c, !num_array[no][2]);
+  digitalWrite(d, !num_array[no][3]);
+  digitalWrite(e, !num_array[no][4]);
+  digitalWrite(f, !num_array[no][5]);
+  digitalWrite(g, !num_array[no][6]);
+  delayMicroseconds(3900);
+  digitalWrite(p, LOW);
+}
 
+void loop()
+{
+  DateTime now = RTC.now();
+  int h = now.hour();
+  int h1 = h % 10;
+  printno(2, h1);
+  printno(1, h / 10);
 
-  
-void loop() {
-   DateTime now = RTC.now();
-  int h=now.hour();
-  int h1=h%10;
-  printno(2,h1);
-  printno(1,h/10);
+  int m = now.minute();
+  int m1 = m % 10;
+  printno(4, m1);
+  printno(3, m / 10);
 
-  int m=now.minute();
-  int m1=m%10;
-  printno(4,m1);
-  printno(3,m/10);
-  
-
-  int s=now.second();
-  int s1=s%10;
-  printno(6,s1);
-  printno(5,s/10);
+  int s = now.second();
+  int s1 = s % 10;
+  printno(6, s1);
+  printno(5, s / 10);
 }
